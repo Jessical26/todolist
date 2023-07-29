eb9700f1fcd290dba54b37f8f98ce4eba30c5c7e
Jessica <jessicaliang26@gmail.com>
2023-07-28 23:28:11 -0700

    add complete todo feature

--- todo/src/components/TodoWrapper.js
+++ todo/src/components/TodoWrapper.js
@@ -11,13 +11,23 @@ export const TodoWrapper = () => {
     setTodos([...todos, {id: uuidv4(), task: todo, completed: false, isEditing: false}]);
     console.log(todos)
   }
+
+  const toggleComplete = id => {
+    setTodos(todos.map(todo => todo.id === id ? {...todo, completed: !todo.completed} : todo ))
+  }
+
+  const deleteTodo = id => {
+    setTodos(todos.filter(todo => todo.id != id ))
+  }
+
   return (
     <div className='TodoWrapper'>
+      <h1>Get Things Done! </h1>
       <TodoForm addTodo={addTodo}/>
       {todos.map((todo, index) => (
-        <Todo task={todo} key={index} />
+        <Todo task={todo} key={index} toggleComplete={toggleComplete} deleteTodo={deleteTodo}/>
       ))} 
-      <Todo />
+      
     </div>
   )
 }
